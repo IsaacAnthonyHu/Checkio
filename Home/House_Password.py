10d90a108807d097b44065d07c0fea8c2658f82b
IsaacAnthonyHu <IsaacAnthonyHu@users.noreply.github.com>
2018-08-08 16:38:56 +0800

    Add House_Password Best Solution

--- Home/House_Password.py
+++ Home/House_Password.py
@@ -20,7 +20,32 @@ def checkio(password):
 	else:
 		return False
 
+#-------------------------------
+DIGIT_RE = re.compile('\d')
+UPPER_CASE_RE = re.compile('[A-Z]')
+LOWER_CASE_RE = re.compile('[a-z]')
 
+def checkio_Best_Solution(data):
+	"""
+	Return True if password strong and False if not
+
+	A password is strong if it contains at least 10 symbols,
+	and one digit, one upper case and one lower case letter.
+	"""
+	if len(data) < 10:
+		return False
+
+	if not DIGIT_RE.search(data):
+		return False
+
+	if not UPPER_CASE_RE.search(data):
+		return False
+
+	if not LOWER_CASE_RE.search(data):
+		return False
+
+	return True
+#-------------------------------
 if __name__ == '__main__':
 	# These "asserts" using only for self-checking and not necessary for auto-testing
 	assert checkio('A1213pokl') == False, "1st example"
