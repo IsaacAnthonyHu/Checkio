6816cec03cade33df9d3ec41f091b431191d2a4a
IsaacAnthonyHu <IsaacAnthonyHu@users.noreply.github.com>
2018-08-08 16:35:52 +0800

    Add House_Password Work

---
+++ Home/House_Password.py
@@ -0,0 +1,32 @@
+# Stephan and Sophia forget about security and use simple passwords for everything.
+# Help Nikola develop a password security check module.
+# Require:
+# The password will be considered strong enough if its length is greater than or equal to 10 symbols.
+# The password has at least one digit, as well as containing one uppercase letter and one lowercase letter in it.
+# The password contains only ASCII latin letters or digits.
+
+import re
+
+
+def checkio(password):
+	a = re.match("[a-zA-Z0-9]+", password)
+	b = re.search("[a-z]", password)
+	c = re.search("[A-Z]", password)
+	d = re.search("[0-9]", password)
+	e = len(password)
+
+	if bool(a) and bool(b) and bool(c) and bool(d) and e >= 10:
+		return True
+	else:
+		return False
+
+
+if __name__ == '__main__':
+	# These "asserts" using only for self-checking and not necessary for auto-testing
+	assert checkio('A1213pokl') == False, "1st example"
+	assert checkio('bAse730onE4') == True, "2nd example"
+	assert checkio('asasasasasasasaas') == False, "3rd example"
+	assert checkio('QWERTYqwerty') == False, "4th example"
+	assert checkio('123456123456') == False, "5th example"
+	assert checkio('QwErTy911poqqqq') == True, "6th example"
+	print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
