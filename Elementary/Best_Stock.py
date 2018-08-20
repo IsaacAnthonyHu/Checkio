9e454dc8ae598b7febc6cd4c9fc8bd771b540902
IsaacAnthonyHu <IsaacAnthonyHu@users.noreply.github.com>
2018-08-20 17:30:41 +0800

    Add The_Warriors Solution

---
+++ Elementary/Best_Stock.py
@@ -0,0 +1,49 @@
+# You are given the current stock prices. You have to find out which stocks cost more.
+#
+# Input:
+# The dictionary where the market identifier code is a key and the value is a stock price.
+#
+# Output:
+# A string and the market identifier code.
+#
+# Example:
+# best_stock({
+#     'CAC': 10.0,
+#     'ATX': 390.2,
+#     'WIG': 1.2
+# }) == 'ATX'
+# best_stock({
+#     'CAC': 91.1,
+#     'ATX': 1.01,
+#     'TASI': 120.9
+# }) == 'TASI'
+# Preconditions:
+# All the prices are unique.
+
+def best_stock(data: dict) -> str:
+	key_list = list(data.keys())
+	value_list = list(data.values())
+	max_value = max(data.values())
+	value_index = value_list.index(max_value)
+	return key_list[value_index]
+
+if __name__ == '__main__':
+	print("Example:")
+	print(best_stock({
+		'CAC': 10.0,
+		'ATX': 390.2,
+		'WIG': 1.2
+	}))
+
+	# These "asserts" are used for self-checking and not for an auto-testing
+	assert best_stock({
+		'CAC': 10.0,
+		'ATX': 390.2,
+		'WIG': 1.2
+	}) == 'ATX', "First"
+	assert best_stock({
+		'CAC': 91.1,
+		'ATX': 1.01,
+		'TASI': 120.9
+	}) == 'TASI', "Second"
+	print("Coding complete? Click 'Check' to earn cool rewards!")
