5e6f86d132b42ce1993a329c2804693381c55594
IsaacAnthonyHu <IsaacAnthonyHu@users.noreply.github.com>
2018-08-21 16:01:02 +0800

    Add Digits_Multiplication Solve

---
+++ Elementary/Digits_Multiplication.py
@@ -0,0 +1,41 @@
+# You are given a positive integer. Your function should calculate the product of the digits excluding any zeroes.
+#
+# For example: The number given is 123405. The result will be 1*2*3*4*5=120 (don't forget to exclude zeroes).
+#
+# Input:
+# A positive integer.
+#
+# Output:
+# The product of the digits as an integer.
+#
+# Example:
+# checkio(123405) == 120
+# checkio(999) == 729
+# checkio(1000) == 1
+# checkio(1111) == 1
+#
+# How it is used:
+# This task can teach you how to solve a problem with simple data type conversion.
+#
+# Precondition:
+# 0 < number < 106
+
+
+def checkio(number: int) -> int:
+	a = 1
+	for x in str(number):
+		if x == '0':
+			pass
+		else:
+			a = a * int(x)
+	return a
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+	assert checkio(123405) == 120
+	assert checkio(999) == 729
+	assert checkio(1000) == 1
+	assert checkio(1111) == 1
+	print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+
